3e5b65acb63839892d461c722b209f71d1241f9f
Patrila Tangwell <111756874+pattang192@users.noreply.github.com>
2024-09-26 22:01:07 -0400

    Implement functionality for user to generate a grid

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const grid = document.querySelector("div");
+const btn = document.querySelector("button");
 
 function createGrid () {
-    for (let i = 0; i < 256; i++) {
-      const row = document.createElement("div");
-      grid.appendChild(row);
-      row.className = "row";        
-    }
+        let num = prompt('How many squares per side? Enter a number from 1 to 100');
+        while (!(num > 0 && num <= 100)) {
+            num = prompt('How many squares per side? Enter a number from 1 to 100');            
+        }
+        
+            for (let i = 0; i < (num); i++) {
+            const row = document.createElement("div");
+            grid.appendChild(row);
+            row.className = "row";
+            for (let j = 0; j < num; j++) {
+                const column = document.createElement("div");
+                row.appendChild(column);
+                column.className = "column";
+                column.addEventListener('mouseenter', () => {
+                    column.style.backgroundColor = 'black'
+                  });  
+            }        
+    }             
 }
-createGrid();
+
+btn.addEventListener('click', () => {
+    grid.replaceChildren();
+    createGrid();
+});
+
+
